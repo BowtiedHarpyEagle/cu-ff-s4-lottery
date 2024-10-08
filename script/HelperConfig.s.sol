4b92888c401e4648d603624f73450e2c8dcf4974
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-10-08 10:50:48 -0500

    complete p23 deploy mock VRF contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,10 +3,15 @@ pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2Mock.sol";
 
 abstract contract CodeConstants {
     uint256 public constant SEPOLIA_NETWORK_ID = 1115511;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
+
+    //Mock Contract Constants
+    uint96 public constant MOCK_BASE_FEE = 0.25 ether;
+    uint96 public constant MOCK_GAS_PRICE_LINK = 1e9;
 }
 
 contract HelperConfig is CodeConstants, Script {
@@ -34,7 +39,7 @@ contract HelperConfig is CodeConstants, Script {
         if (networkConfigs[chainId].vrfCoordinator != address(0)) {
             return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
-            //return getOrCreateAnvilEthConfig();
+            return getOrCreateAnvilEthConfig();
         } else {
             revert HelperConfig__InvalidChainId();
         }
@@ -57,5 +62,26 @@ contract HelperConfig is CodeConstants, Script {
         if (networkConfigs[LOCAL_CHAIN_ID].vrfCoordinator != address(0)) {
             return localNetworkConfig;
         }
+
+        // if not, create it using mock contract
+
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorMock = new VRFCoordinatorV2Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK
+        );
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30, // 30 seconds
+            vrfCoordinator: address(vrfCoordinatorMock),
+            // gas doesn't matter in mock contract
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callbackGasLimit: 500000, // 500,000 gas
+            subscriptionId: 0 // we may need to fix this later
+        });
+
+        return localNetworkConfig;
     }
 }
