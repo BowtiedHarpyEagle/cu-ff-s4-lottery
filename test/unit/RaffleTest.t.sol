5711fa720d99cc4c48dce1dc005810e9e3f21e26
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-10-09 21:40:54 -0500

    p36 2:15 test 1

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -109,4 +109,18 @@ contract RaffleTest is Test {
         //Assert
         assert(!upkeepNeeded);
     }
+
+    /**
+     * Test Perform Upkeep
+     */
+
+    function testPerformUpkeepRunsOnlyWhenCheckUpkeepReturnsTrue() public {
+        //Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        //Act - Assert
+        raffle.performUpkeep("");
+    }
 }
